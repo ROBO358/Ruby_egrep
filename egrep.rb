87cb17ff53e5bae62dcb1065c87d4e1fb16809cb
ROBO358 <18186312+ROBO358@users.noreply.github.com>
2022-10-28 20:29:39 +0900

    Fix:egrepをクラス化
    クラス化と同時に各種処理をメソッド化した

--- egrep.rb
+++ egrep.rb
@@ -1,31 +1,86 @@
-print ARGV[1], "ファイルより、正規表現(/", ARGV[0], "/)を実行します。\n"
+class EGREP
+  def initialize
+    # コマンドライン引数をわかりやすく変数へ
 
-# コマンドライン引数をわかりやすく変数へ
-filename = ARGV[1].to_s
+    # ファイル名を取得
+    _err, @filename = filename_extraction(ARGV)
+    if !_err.nil?
+      puts _err
+      exit(-1)
+    end
 
-begin
-  regexp = Regexp.new(ARGV[0])
-rescue
-  print($!.to_s + "\n")
-  exit(-1)
-end
+    # 正規表現を取得
+    _err, @regexp = regexp_extraction(ARGV)
+    if !_err.nil?
+      puts _err
+      exit(-1)
+    end
 
-begin
-  # 検索対象の文章をファイルから読み込み
-  f = File.open(filename, "r")
-rescue
-  print($!.to_s + "\n")
-  exit(-1)
-end
+    # ファイルを読み込み
+    _err, @file = file_loading(@filename)
+    if !_err.nil?
+      puts _err
+      exit(-1)
+    end
 
-# 一行ずつ照会
-f.each{|line|
-  # 一致する場合
-  if !(regexp =~ line).nil? # 正規表現を照会した結果indexが帰ってきた場合
-    # 一致する行を出力
-    print line
+    # 検索
+    matched_array = search(@file, @regexp)
+
+    # 一致した行を出力
+    puts matched_array
+
+    # ファイルを閉じる
+    @file.close
+
   end
-}
 
-# ファイルを閉じる
-f.close
+  def filename_extraction(argv)
+    _err = nil
+    filename = nil
+    begin
+      filename = argv[1].to_s
+    rescue
+      _err = $!
+    end
+    return _err, filename
+  end
+
+  def regexp_extraction(argv)
+    _err = nil
+    regexp = nil
+    begin
+      regexp = Regexp.new(argv[0])
+    rescue
+      _err = $!
+    end
+    return _err, regexp
+  end
+
+  def file_loading(filename)
+    _err = nil
+    file = nil
+    begin
+      # 検索対象の文章をファイルから読み込み
+      file = File.open(filename, "r")
+    rescue
+      _err = $!
+    end
+    return _err, file
+  end
+
+  def search(file, regexp)
+    matched_array = []
+    print file.path, "ファイルより、正規表現(/", regexp.source, "/)を実行します。\n"
+    # 一行ずつ照会
+    file.each{|line|
+      # 一致する場合
+      if !(regexp =~ line).nil? # 正規表現を照会した結果indexが帰ってきた場合
+        # 一致する行を出力
+        matched_array.append(line)
+      end
+    }
+    return matched_array
+  end
+end
+
+EGREP.new
